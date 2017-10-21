ca06392171cfd10d0b9011cb458b0e7a72b02bc6
sunshine <94733713@qq.com>
2017-10-21 23:39:11 +0800

    线上环境关闭注册路由

---
+++ app/Http/Controllers/Test/kafkaController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Kafka;
+
+class kafkaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $config = \Kafka\ProducerConfig::getInstance();
+        $config->setMetadataBrokerList('60.205.183.42:9092');
+
+        $producer = new \Kafka\Producer(function() {
+            return array(
+                array(
+                    'topic' => 'sun.huajie',
+                    'value' => 'test....message.',
+                    'key' => 'testkey',
+                ),
+            );
+        });
+        $producer->success(function($result) {
+            var_dump($result);
+        });
+        $producer->error(function($errorCode) {
+            var_dump($errorCode);
+        });
+        $producer->send(true);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
